0c0579d95e6005a09eb3d16c87cdfc48c7207814
Prerna Prasad <prernaprasad100@gmail.com>
2023-05-29 21:14:37 +0530

    add wishlist.html & update product card styles

---
+++ js/data.js
@@ -0,0 +1,28 @@
+
+    // Your module code goes here
+    var productArray = [
+        {id: '0', name: 'Honey Soap bar', src:'img/nature-petal-honey-soap.jpg', price:'200',discount:'10'},
+        {id: '1', name: 'Avocado Soap bar', src:'img/nature-petal-avocado-soap.jpg', price:'200',discount:'10'},
+        {id: '2', name: 'Cherry Blossom Body Lotion', src:'img/nature-petal-cherry-blossom-lotion.jpg', price:'200',discount:'20'},
+        {id: '3', name: 'Green Tea Body Lotion', src:'img/nature-petal-green-tea-lotion.jpg', price:'200',discount:'10'},
+        {id: '4', name: 'Honey Soap bar', src:'img/nature-petal-honey-soap.jpg', price:'200',discount:'0'},
+        {id: '5', name: 'Avocado Soap bar', src:'img/nature-petal-avocado-soap.jpg', price:'200',discount:'20'},
+        {id: '6', name: 'Honey Soap bar', src:'img/nature-petal-honey-soap.jpg', price:'200',discount:'20'},
+        {id: '7', name: 'Green Tea Body Lotion', src:'img/nature-petal-green-tea-lotion.jpg', price:'200',discount:'0'},
+        {id: '8', name: 'Cherry Blossom Body Lotion', src:'img/nature-petal-cherry-blossom-lotion.jpg', price:'200',discount:'20'},
+        {id: '9', name: 'Avocado Soap bar', src:'img/nature-petal-avocado-soap.jpg', price:'200',discount:'0'},
+        {id: '10', name: 'Green Tea Body Lotion', src:'img/nature-petal-green-tea-lotion.jpg', price:'200',discount:'20'},
+        {id: '11', name: 'Honey Soap bar', src:'img/nature-petal-honey-soap.jpg', price:'200',discount:'10'},
+        {id: '12', name: 'Cherry Blossom Body Lotion', src:'img/nature-petal-cherry-blossom-lotion.jpg', price:'200',discount:'20'},
+        {id: '13', name: 'Avocado Soap bar', src:'img/nature-petal-avocado-soap.jpg', price:'200',discount:'10'},
+        {id: '14', name: 'Honey Soap bar', src:'img/nature-petal-honey-soap.jpg', price:'200',discount:'20'},
+        {id: '15', name: 'Green Tea Body Lotion', src:'img/nature-petal-green-tea-lotion.jpg', price:'200',discount:'20'},
+        {id: '16', name: 'Cherry Blossom Body Lotion', src:'img/nature-petal-cherry-blossom-lotion.jpg', price:'200',discount:'20'},
+        {id: '17', name: 'Honey Soap bar', src:'img/nature-petal-honey-soap.jpg', price:'200',discount:'20'},
+        {id: '18', name: 'Avocado Soap bar', src:'img/nature-petal-avocado-soap.jpg', price:'200',discount:'20'},
+        {id: '19', name: 'Cherry Blossom Body Lotion', src:'img/nature-petal-cherry-blossom-lotion.jpg', price:'200',discount:'20'},
+        {id: '20', name: 'Cherry Blossom Body Lotion', src:'img/product1.png', price:'200',discount:'20'},
+    ];
+
+
+
