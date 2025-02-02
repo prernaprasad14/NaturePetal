abbc1ca4d4360d880f25e72103521095bef5ca15
Prerna Prasad <prernaprasad100@gmail.com>
2025-02-02 23:32:14 +0530

    add : assets and readme

--- js/loader.js
+++ js/loader.js
@@ -1,22 +1,8 @@
-
-// function disableScroll() {
-//     document.body.style.overflow = "hidden";
-// }
-
-// // Function to enable scrolling
-// function enableScroll() {
-//     document.body.style.overflow = "auto";
-// }
-
-// // Call disableScroll when page starts loading
-// disableScroll();
-
 // Simulate loading process
 window.addEventListener("load", function () {
     const mainLoader= document.getElementById("main-loader");
     mainLoader.classList.add("fade-out");
     setTimeout(() => {
         mainLoader.style.display = "none"; // Hide loader
-        // enableScroll(); 
     }, 3000);
 });
---
+++ readme.md
@@ -0,0 +1,8 @@
+This is my case project on branding and packaging for the brand : Nature Petal
+
+Tech used : HTML, CSS3, Bootstrap, Javascript, Jquery
+Deployed on Netlify : https://naturepetal.netlify.app/
+
+For Brand Identity and Packaging : 
+1. https://dribbble.com/shots/21512983-Brand-Identity-and-Packaging-Nature-Petal
+2. https://dribbble.com/shots/21411648-Nature-Petal-Store
